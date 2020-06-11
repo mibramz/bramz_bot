68f1681a8a94df810750a9a00b29c506e7a11850
Bramz <65383844+mibramz@users.noreply.github.com>
2020-06-11 10:21:23 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+// Edit these values if you're stealing my code and using this bot in your channel.
+// myChannel = Twitch channel you want your bot to send messages in.
+// cdownTime = In seconds, time that must pass in between each use of commands.
+// timerInterval = In minutes, time that must pass before each execution of timer commands.
+// messageLimit = How many messages must happen before each execution of timer commands.
+// twitter, discord, youtube, instagram = Your links to these respective sites (everything after 'https://twitter.com/', for example).
+// useTimer = Whether or not you want to use the timer commands.
+
+module.exports = {
+  myChannel: 'Bramz',
+  cdownTime: 10,
+  timerInterval: 20,
+  messageLimit: 10,
+  twitter: '__Bramz',
+  discord: 'Bramz',
+  youtube: 'c/BramzYT',
+  instagram: '_bramz',
+  speedruncom: 'Bramz',
+  useTimer: true
+};
